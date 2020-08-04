76051e069df68b3505329cb1587edb7cfa4272ba
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-08-04 17:59:54 +0200

    Create Full Screen Feature

--- stage/js/main.js
+++ stage/js/main.js
@@ -12,4 +12,48 @@ $(function () {
     $(this).toggleClass("open-submenu");
     $(".child-links").slideToggle();
   });
+
+  // Toggle Full Screen
+  $('.fullscreen').on('click', function () {
+    console.log('dd')
+    $(this).toggleClass('isFull');
+
+    if ($(this).hasClass('isFull')) {
+      openFullScreen();
+    } else {
+      closeFullScreen();
+    }
+
+    function openFullScreen() {
+      var elem = document.documentElement;
+
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.mozRequestFullScreen) {
+        /* Firefox */
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) {
+        /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) {
+        /* IE/Edge */
+        elem.msRequestFullscreen();
+      }
+    }
+
+    function closeFullScreen() {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        /* Firefox */
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        /* IE/Edge */
+        document.msExitFullscreen();
+      }
+    }
+  })
 });
